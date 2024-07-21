f7d1848dd0c9ed621475ba0c19275f4ef596ff15
Lysenko <lysenkoden@gmail.com>
2024-07-21 07:33:21 +0200

    users still unknown because of lag loading

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/api/apiSlice';
-import usersReducer from '../features/users/usersSlice';
-import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    users: usersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
